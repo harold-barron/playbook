ca06505df5bf4d4e53a88f261b889df3f11e0fc7
Harold Barron <haroldbarron13@gmail.com>
2022-04-20 18:14:14 -0500

    Creating pokemon.js

---
+++ weekly_mission_1/example10/pokemon.js
@@ -0,0 +1,16 @@
+/*
+    import MyPokemon from './pokemon.js'
+*/
+
+export default class 
+{
+    constructor(name)
+    {
+        this.name = name
+    }
+
+    sayHello(message)
+    {
+        console.log(`${this.name} says ${message}`)
+    }
+}
