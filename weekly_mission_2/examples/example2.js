bb87a0d1d8e12886d9771262248d1e8122e8def8
Harold Barron <haroldbarron13@gmail.com>
2022-04-20 22:49:18 -0500

    Using filter and includes

--- weekly_mission_2/examples/example2.js
+++ weekly_mission_2/examples/example2.js
@@ -71,7 +71,8 @@ const explorerStack=explorers.map(ExplorerStackNewList => ExplorerStackNewList.s
 console.log(explorerStack)
 
 // Obtén la lista de explorers que tengan en su stack "js", usa FILTER (para validar un elemento en un lista se usa el método includes)
-
+const explorerWithJS = explorers.filter ((explorerJS) => explorerJS.stack.includes('js'))
+console.log(explorerWithJS)
 // Busca el primer explorer que sea de la CDMX, usa FIND
 // Obtén la suma de todos los exercises_completed, usa REDUCE
 // Obtén la validación si al menos uno de los explorers tiene la propiedad exercisesFinished en frontend como true, usa SOME
