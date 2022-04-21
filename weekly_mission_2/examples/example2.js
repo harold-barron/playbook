5be2bc54a0a7cfa38c107f93c46afaf670c6f1a3
Harold Barron <haroldbarron13@gmail.com>
2022-04-20 22:12:06 -0500

    Using forEach to print every explorer name in the list

--- weekly_mission_2/examples/example2.js
+++ weekly_mission_2/examples/example2.js
@@ -58,3 +58,16 @@ const explorers = [
       }
     }
    ]
+
+// Exercises
+// 1.-Imprime el nombre (propiedad name) de cada explorer en la lista, usa FOR EACH
+explorers.forEach(expName => console.log(expName.name))
+
+
+// Imprime el stack de cada explorer, usa FOR EACH
+// Crea una nueva lista con las listas de stacks de cada explorer, usa MAP
+// Obtén la lista de explorers que tengan en su stack "js", usa FILTER (para validar un elemento en un lista se usa el método includes)
+// Busca el primer explorer que sea de la CDMX, usa FIND
+// Obtén la suma de todos los exercises_completed, usa REDUCE
+// Obtén la validación si al menos uno de los explorers tiene la propiedad exercisesFinished en frontend como true, usa SOME
+// Obtén la validación si todos los explorers tienen la propiedad isFinished del onboarding como true. Usa EVERY.
